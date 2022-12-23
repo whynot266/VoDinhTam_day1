10358ba8ffdb194d714afcd02dfeed2f96d463d9
Wykin <phanvanky76@gmail.com>
2022-12-23 18:23:31 +0700

    Fix 2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context= new ClassPathXmlApplicationContext("beans");
+        Account account1= context.getBean("account1", Account.class);
+        Account account2= context.getBean("account2", Account.class);
+        System.out.println(account2.getBalance());
+        System.out.println(account1.getOwnerName());
+        System.out.println(account1.getBalance());
+    }
+}
